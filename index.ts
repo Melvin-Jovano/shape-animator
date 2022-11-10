03db7d5c6376692dd3a93e176a83da57b2553f71
Melvin-Jovano <melvinjovano2@gmail.com>
2022-11-10 08:09:29 +0700

    add frame per second functionality

--- index.ts
+++ index.ts
@@ -1,24 +1,32 @@
-import { rows, columns, frames } from "./data";
+import { rows, columns, frames, framesPerSecond } from "./data";
 import { print } from "./util";
 
+let timer: number = 1;
+
 frames.forEach(frame => {
-    for (let row = 1; row <= rows; row++) {
+
+    setTimeout(() => {
+        console.clear();
+        for (let row = 1; row <= rows; row++) {
         
-        // TODO: Debugging Purpose
-        process.stdout.write(`${row}\t`);
-        // ENDTODO
-        
-        for (let column = 1; column <= columns; column++) {
-            if(Object.keys(frame).map(key => Number(key)).includes(row)) {
-                let isDisabled = false;
-                frame[String(row)].forEach(disabledColumn => {
-                    if(column >= disabledColumn[0] && column <= disabledColumn[1]) isDisabled = true;
-                });
-                if(isDisabled) print(column, true);
-                else print(column);
-            } else {
-                print(column);
+            // TODO: Debugging Purpose
+            process.stdout.write(`${row}\t`);
+            // ENDTODO
+    
+            for (let column = 1; column <= columns; column++) {
+                if(Object.keys(frame).map(key => Number(key)).includes(row)) {
+                    let isDisabled = false;
+                    frame[String(row)].forEach(disabledColumn => {
+                        if(column >= disabledColumn[0] && column <= disabledColumn[1]) isDisabled = true;
+                    });
+                    if(isDisabled) print(column, true);
+                    else print(column);
+                } else {
+                    print(column);
+                }
             }
         }
-    }
+    }, (1000 / framesPerSecond) * timer);
+
+    timer++;
 });
