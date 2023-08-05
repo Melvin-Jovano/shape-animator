b84d628148732789348b6c5b6db0cffe6ade7ff3
Melvin-Jovano <melvinjovano2@gmail.com>
2023-08-05 23:39:22 +0700

    feat: update frames

--- data.ts
+++ data.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
-export const rows: number = 105;
-export const columns: number = 335;
+export const rows: number = 76;
+export const columns: number = 315;
 export const framesPerSecond: number = 5;
 export const shape: string = '*';
 export const frames: Record<string, number[][]>[] = JSON.parse(fs.readFileSync('frames/jjk-ed.json', {encoding: 'utf-8'}));
--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-import { rows, columns, frames, framesPerSecond } from "./data";
+import { rows, columns, frames, framesPerSecond, shape } from "./data";
 import { print } from "./util";
 
 let timer: number = 1;
 
 frames.forEach(frame => {
-
     setTimeout(() => {
         console.clear();
         for (let row = 1; row <= rows; row++) {
@@ -13,18 +12,19 @@ frames.forEach(frame => {
             process.stdout.write(`${row}\t`);
             // ENDTODO
     
+            let rowData = '';
             for (let column = 1; column <= columns; column++) {
                 if(Object.keys(frame).map(key => Number(key)).includes(row)) {
                     let isDisabled = false;
                     frame[String(row)].forEach(disabledColumn => {
                         if(column >= disabledColumn[0] && column <= disabledColumn[1]) isDisabled = true;
                     });
-                    if(isDisabled) print(column, true);
-                    else print(column);
+                    rowData += isDisabled ? ' ' : shape;
                 } else {
-                    print(column);
+                    rowData += shape;
                 }
             }
+            process.stdout.write(rowData + "\n");
         }
     }, (1000 / framesPerSecond) * timer);
 
